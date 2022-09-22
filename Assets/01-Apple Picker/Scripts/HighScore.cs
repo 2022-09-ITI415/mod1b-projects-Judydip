a8f597454fdbaae75116615db3845e0cd484f750
Judy Y <42835984+Judydip@users.noreply.github.com>
2022-09-22 19:08:29 -0400

    Apple Picker Playable Game

--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -6,6 +6,14 @@ using UnityEngine.UI;
 public class HighScore : MonoBehaviour
 {
     static public int score = 1000;
+
+    void Awake (){
+        if (PlayerPrefs.HasKey("HighScore")){
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +25,9 @@ public class HighScore : MonoBehaviour
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+
+        if (score > PlayerPrefs.GetInt("HighScore")){
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
